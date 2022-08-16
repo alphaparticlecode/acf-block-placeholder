df1d2252ca25643f24ce11e0131abd6e783b8f38
Keanan Koppenhaver <keanan@alphaparticle.com>
2022-08-16 01:31:29 -0500

    Make just-created blocks work (pre-save)

--- acf-block-placeholder.php
+++ acf-block-placeholder.php
@@ -17,105 +17,105 @@
 
 class ACF_Block_Placeholder {
 
-    /**
-     * Static property to hold our singleton instance
-     *
-     */
-    static $instance = false;
+	/**
+	 * Static property to hold our singleton instance
+	 *
+	 */
+	static $instance = false;
 
-    /**
-     * This is our constructor
-     *
-     * @return void
-     */
-    private function __construct() {
-        add_filter('acf/register_block_type_args', [ $this, 'my_acf_register_block_type_args' ] );
+	/**
+	 * This is our constructor
+	 *
+	 * @return void
+	 */
+	private function __construct() {
+		add_filter('acf/register_block_type_args', [ $this, 'my_acf_register_block_type_args' ] );
 
-        add_action('acf/input/admin_enqueue_scripts', [ $this, 'my_acf_admin_enqueue_scripts' ] );
-    }
+		add_action('acf/input/admin_enqueue_scripts', [ $this, 'my_acf_admin_enqueue_scripts' ] );
+	}
 
-    /**
-     * If an instance exists, this returns it.  If not, it creates one and
-     * retuns it.
-     *
-     * @return ACF_Block_Placeholder
-     */
+	/**
+	 * If an instance exists, this returns it.  If not, it creates one and
+	 * retuns it.
+	 *
+	 * @return ACF_Block_Placeholder
+	 */
 
-    public static function getInstance() {
-        if ( !self::$instance ) {
-            self::$instance = new self;
-        }
-        return self::$instance;
-    }
+	public static function getInstance() {
+		if ( !self::$instance ) {
+			self::$instance = new self;
+		}
+		return self::$instance;
+	}
 
-    public function my_acf_register_block_type_args( $args ){
-        //TO-DO: Figure out a way to pass the already-registered callback
-        // as a param to our function so it can be called after our
-        // work is done and we can support blocks with callbacks instead of templates
+	public function my_acf_register_block_type_args( $args ){
+		//TO-DO: Figure out a way to pass the already-registered callback
+		// as a param to our function so it can be called after our
+		// work is done and we can support blocks with callbacks instead of templates
 
-        // If this block is already using a render callback,
-        // we don't want to mess with that.
-        if( empty( $args['render_callback'] ) ) {
-            $args['render_callback'] = [$this, 'maybe_generate_acf_block_placeholder'];
-        }
-        
-        return $args;
-    }
+		// If this block is already using a render callback,
+		// we don't want to mess with that.
+		if( empty( $args['render_callback'] ) ) {
+			$args['render_callback'] = [$this, 'maybe_generate_acf_block_placeholder'];
+		}
+		
+		return $args;
+	}
 
-    function maybe_generate_acf_block_placeholder( $block, $content = '', $is_preview = false, $post_id = 0 ) {
+	public function maybe_generate_acf_block_placeholder( $block, $content = '', $is_preview = false, $post_id = 0 ) {
 
-        if( $is_preview && $this->is_block_empty( $block ) ) {
-            echo '<div class="acf-block-placeholder"><div><h1>' . $block['title'] . '</h1><p>This block is currently empty. Switch to edit mode or add data using the sidebar.</p></div></div>';
-        } else {
-            // Call the block template as normal
-            // Locate template.
-            if( file_exists($block['render_template']) ) {
-                $path = $block['render_template'];
-            } else {
-                $path = locate_template( $block['render_template'] );
-            }
-            
-            // Include template.
-            if( file_exists($path) ) {
-                include( $path );
-            }
-        }
-    }
+		if( $is_preview && $this->is_block_empty( $block ) ) {
+			echo '<div class="acf-block-placeholder"><div><h1>' . $block['title'] . '</h1><p>This block is currently empty. Switch to edit mode or add data using the sidebar.</p></div></div>';
+		} else {
+			// Call the block template as normal
+			// Locate template.
+			if( file_exists($block['render_template']) ) {
+				$path = $block['render_template'];
+			} else {
+				$path = locate_template( $block['render_template'] );
+			}
+			
+			// Include template.
+			if( file_exists($path) ) {
+				include( $path );
+			}
+		}
+	}
 
-    function is_block_empty( $block ) {
-        $fields    = acf_get_block_fields($block);
-        $field_ids = wp_list_pluck( $fields, 'key' );
+	private function is_block_empty( $block ) {
+		$fields    = acf_get_block_fields($block);
+		$field_ids = wp_list_pluck( $fields, 'key' );
 
-        $block_data = array_chunk($block['data'], 2);
+		$block_data = array_chunk($block['data'], 2);
 
-        foreach( $block_data as $field_data ) {
-            $current_value = $field_data[0];
-            $field_key = $field_data[1];
+		foreach( $block_data as $field_data ) {
+			$current_value = $field_data[0];
+			$field_key = $field_data[1];
 
-            if( empty( $current_value ) ) {
-                // If the field doesn't have anything in it, we can move on.
-                continue;
-            } else {
-                // Otherwise, it gets a bit more complicated and we need
-                // to check if the current value of the field is equal
-                // to the default for this field to be considered "empty".
-                $field_key_index = array_search( $field_key, $field_ids );
-                
-                $field_default_value = $fields[$field_key_index];
+			if( empty( $current_value ) || false !== strpos( $current_value, 'field_' ) ) {
+				// If the field doesn't have anything in it, we can move on.
+				continue;
+			} else {
+				// Otherwise, it gets a bit more complicated and we need
+				// to check if the current value of the field is equal
+				// to the default for this field to be considered "empty".
+				$field_key_index = array_search( $field_key, $field_ids );
+				
+				$field_default_value = $fields[$field_key_index];
 
-                if( empty( $fields[$field_key_index]['default_value'] ) || $current_value != $fields[$field_key_index]['default_value'] ) {
-                    return false;
-                }
-            }
-        }
+				if( empty( $fields[$field_key_index]['default_value'] ) || $current_value != $fields[$field_key_index]['default_value'] ) {
+					return false;
+				}
+			}
+		}
 
-        return true;
-    }
+		return true;
+	}
 
-    function my_acf_admin_enqueue_scripts() {
-        $plugin_url = plugin_dir_url( __FILE__ );
-        wp_enqueue_style( 'acf-placeholder-css', $plugin_url . 'css/acf-placeholder.css', false, '1.0.0' );
-    }
+	public function my_acf_admin_enqueue_scripts() {
+		$plugin_url = plugin_dir_url( __FILE__ );
+		wp_enqueue_style( 'acf-placeholder-css', $plugin_url . 'css/acf-placeholder.css', false, '1.0.0' );
+	}
 }
 
 $ACF_Block_Placeholder = ACF_Block_Placeholder::getInstance();
